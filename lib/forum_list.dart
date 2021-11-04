e244b2007d7ca86488d30089d630e406a9c54c53
kai <18307130141@fudan.edu.cn>
2021-11-04 21:44:16 +0800

    整理了代码，删除无用icon和副标题。

--- lib/forum_list.dart
+++ lib/forum_list.dart
@@ -1,11 +1,8 @@
 import 'dart:async';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/read.dart';
 import 'package:myapp/utils/global_value.dart';
-import 'package:myapp/utils/note_db_helper.dart';
-
 import 'entity/note.dart';
 
 
@@ -13,7 +10,7 @@ class ListPage extends StatefulWidget {
   ListPage();
   @override
   State<StatefulWidget> createState() {
-    print("runrun");
+    // print("runrun");
     return ListPageState();
   }
 }
@@ -24,19 +21,6 @@ class ListPageState extends State<ListPage> with AutomaticKeepAliveClientMixin {
   int _size = 0;
   final List<Note> _noteList = List.empty(growable: true);
   //StreamSubscription subscription;
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   // 注册和监听t发送来的UserEven类型事件、数据
-  //   subscription = eventBus.on<NoteEvent>().listen((NoteEvent event) {
-  //     _onRefresh();
-  //   });
-  //   _scrollController.addListener(() {
-  //     ///滚动监听
-  //   });
-  //   _onRefresh();
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -84,49 +68,9 @@ class ListPageState extends State<ListPage> with AutomaticKeepAliveClientMixin {
   }
 
   Future<Null> nouse() async{
-    print("nouse");
+    // print("nouse");
     return null;
   }
-  // _showBottomSheet(int index, BuildContext c) {
-  //   showModalBottomSheet(
-  //       context: context,
-  //       builder: (BuildContext context) {
-  //         return new Column(
-  //           mainAxisSize: MainAxisSize.min,
-  //           children: <Widget>[
-  //             ListTile(
-  //               leading: Icon(Icons.content_copy),
-  //               title: Text("复制"),
-  //               onTap: () async {
-  //                 Clipboard.setData(
-  //                     ClipboardData(text: _noteList.elementAt(index).content));
-  //                 Scaffold.of(c).showSnackBar(SnackBar(
-  //                   content: Text("已经复制到剪贴板"),
-  //                   backgroundColor: Colors.black87,
-  //                   duration: Duration(
-  //                     seconds: 2,
-  //                   ),
-  //                 ));
-  //                 Navigator.pop(context);
-  //               },
-  //             ),
-  //             ListTile(
-  //               leading: Icon(Icons.delete_sweep),
-  //               title: Text("删除"),
-  //               onTap: () async {
-  //                 widget.noteDbHelpter
-  //                     .deleteById(_noteList.elementAt(index).id);
-  //                 setState(() {
-  //                   _noteList.removeAt(index);
-  //                   _size = _noteList.length;
-  //                 });
-  //                 Navigator.pop(context);
-  //               },
-  //             ),
-  //           ],
-  //         );
-  //       });
-  // }
 
   //刷新
   Future<void> _onRefresh() async {
@@ -150,7 +94,7 @@ class ListPageState extends State<ListPage> with AutomaticKeepAliveClientMixin {
   }
 
   Widget getItem(int index) {
-    print("item");
+    // print("item");
     return Container(
       child: Card(
         margin: EdgeInsets.fromLTRB(10, 8, 10, 8),
@@ -172,42 +116,7 @@ class ListPageState extends State<ListPage> with AutomaticKeepAliveClientMixin {
                             fontSize: 50,
                             fontWeight: FontWeight.normal),
                       ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            "bbbbbbbb",
-                            //'星期${TimeUtils.getWeekday(DateTime.fromMillisecondsSinceEpoch(_noteList.elementAt(index).time).weekday)}',
-                            style: TextStyle(
-                                color: Color.fromRGBO(149, 149, 148, 1),
-                                fontSize: 18),
-                          ),
-                          Text(
-                            "31",
-                            // TimeUtils.getDate(
-                            //     DateTime.fromMillisecondsSinceEpoch(
-                            //         _noteList.elementAt(index).time)),
-                            style: TextStyle(
-                                color: Color.fromRGBO(149, 149, 148, 1),
-                                fontSize: 18),
-                          ),
-                        ],
-                      ),
                     ],
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    alignment: Alignment.centerRight,
-                    padding: EdgeInsets.fromLTRB(0, 5, 20, 5),
-                    child: Icon(
-                      Icons.wb_sunny,
-                      size: 50,
-                      color: Color.fromRGBO(252, 205, 24, 1),
-                    ),
                   ),
                 ),
               ],
@@ -229,106 +138,6 @@ class ListPageState extends State<ListPage> with AutomaticKeepAliveClientMixin {
       ),
     );
   }
-  //
-  // Widget getImageItem(int index) {
-  //   return Container(
-  //     child: Card(
-  //       margin: EdgeInsets.fromLTRB(10, 8, 10, 8),
-  //       child: Column(
-  //         crossAxisAlignment: CrossAxisAlignment.start,
-  //         children: <Widget>[
-  //           Row(
-  //             mainAxisAlignment: MainAxisAlignment.spaceAround,
-  //             children: <Widget>[
-  //               Container(
-  //                 padding: EdgeInsets.fromLTRB(20, 10, 20, 5),
-  //                 child: Row(
-  //                   children: <Widget>[
-  //                     Text(
-  //                       _noteList.length == 0
-  //                           ? ''
-  //                           : '${DateTime.fromMillisecondsSinceEpoch(_noteList.elementAt(index).time).day}',
-  //                       style: TextStyle(
-  //                           color: Color.fromRGBO(52, 52, 54, 1),
-  //                           fontSize: 50,
-  //                           fontWeight: FontWeight.normal),
-  //                     ),
-  //                     SizedBox(
-  //                       width: 5,
-  //                     ),
-  //                     Column(
-  //                       crossAxisAlignment: CrossAxisAlignment.start,
-  //                       children: <Widget>[
-  //                         Text(
-  //                           _noteList.length == 0
-  //                               ? ''
-  //                               : '星期${TimeUtils.getWeekday(DateTime.fromMillisecondsSinceEpoch(_noteList.elementAt(index).time).weekday)}',
-  //                           style: TextStyle(
-  //                               color: Color.fromRGBO(149, 149, 148, 1),
-  //                               fontSize: 18),
-  //                         ),
-  //                         Text(
-  //                           _noteList.length == 0
-  //                               ? ''
-  //                               : TimeUtils.getDate(
-  //                               DateTime.fromMillisecondsSinceEpoch(
-  //                                   _noteList.elementAt(index).time)),
-  //                           style: TextStyle(
-  //                               color: Color.fromRGBO(149, 149, 148, 1),
-  //                               fontSize: 18),
-  //                         ),
-  //                       ],
-  //                     ),
-  //                   ],
-  //                 ),
-  //               ),
-  //               Expanded(
-  //                 child: Container(
-  //                   alignment: Alignment.centerRight,
-  //                   padding: EdgeInsets.fromLTRB(0, 5, 20, 5),
-  //                   child: Icon(
-  //                     Icons.wb_sunny,
-  //                     size: 50,
-  //                     color: Color.fromRGBO(252, 205, 24, 1),
-  //                   ),
-  //                 ),
-  //               ),
-  //             ],
-  //           ),
-  //           Container(
-  //               margin: EdgeInsets.all(10),
-  //               child: Row(
-  //                 crossAxisAlignment: CrossAxisAlignment.start,
-  //                 children: <Widget>[
-  //                   ClipRRect(
-  //                     borderRadius: BorderRadius.circular(5.0),
-  //                     child: Image.network(
-  //                       'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564678338847&di=ab19cbea9b5d88a9969ce0825ac5c84d&imgtype=0&src=http%3A%2F%2Fattachments.gfan.net.cn%2Fforum%2F201501%2F13%2F143316yttiiyiuvufcoyjh.jpg',
-  //                       height: 108,
-  //                       width: 108,
-  //                       fit: BoxFit.cover,
-  //                     ),
-  //                   ),
-  //                   Expanded(
-  //                       child: Container(
-  //                         padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-  //                         child: Text(
-  //                           _noteList.elementAt(index).content,
-  //                           maxLines: 4,
-  //                           overflow: TextOverflow.ellipsis,
-  //                           style: TextStyle(
-  //                             color: Color.fromRGBO(103, 103, 103, 1),
-  //                             fontSize: 18,
-  //                           ),
-  //                         ),
-  //                       )),
-  //                 ],
-  //               )),
-  //         ],
-  //       ),
-  //     ),
-  //   );
-  // }
 
   @override
   void dispose() {
@@ -339,17 +148,17 @@ class ListPageState extends State<ListPage> with AutomaticKeepAliveClientMixin {
   @override
   bool get wantKeepAlive => true;
   void debugMode() {
-    //DbUtil.noteDbHelper.deleteById(0);
-
+    // DbUtil.noteDbHelper.deleteById(0);
+    //
     Note note = Note.fromMap({
-      columnTitle:"testTitle",
+      columnTitle:"圣经第一章",
       columnContent:"起初　神创造天地。1:2 地是空虚混沌．渊面黑暗．神的灵运行在水面上1:3 神说、要有光、就有了光。1:4 神看光是好的、就把光暗分开了。",
       columnTime:DateTime.now().millisecondsSinceEpoch,
       columnReply:2,
       columnReplyId:0,
       columnPostId:0});
 
-    //DbUtil.noteDbHelper.insert(note);
-    _onRefresh();
+     DbUtil.noteDbHelper.insert(note);
+    // _onRefresh();
   }
 }
--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/syllabus_home.dart';
@@ -69,5 +68,4 @@ class HomePageState extends State<HomePage> {
       }
     });
   }
-
 }
